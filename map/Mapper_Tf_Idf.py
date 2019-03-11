76b27755bd5565412e9fa425b04d60a2b3bf9c96
khalfaoui <kaiim@MacBook-Pro-de-khalfaoui.local>
2019-03-11 16:29:42 +0100

    reduce_1 tf-idf ok

--- map/Mapper_Tf_Idf.py
+++ map/Mapper_Tf_Idf.py
@@ -4,12 +4,16 @@ import sys
 import os
 
 #Importer du fichier des mots a ne pas compter
-#path_file_stopword = "/Users/kaiim/project/MapReduce/files/tf-idf/stopwords_en.txt"
+path_file_stopword = "/Users/kaiim/project/MapReduce/tf-idf/files/stopwords_en.txt"
+path_file_stopword = "/Users/kaiim/project/MapReduce/tf-idf/files/stopwords_en.txt"
 
-path_file_stopword = "./tfidf/files/stopwords_en.txt"
 
+
+
+#path_file_stopword = "./tfidf/files/stopwords_en.txt"
 fd = open(path_file_stopword, 'r') 
 stopwords = fd.read().strip().split()
+
 
 
 
@@ -17,17 +21,17 @@ stopwords = fd.read().strip().split()
 for line in sys.stdin:
         # Pour tester en local -> export de la variable s3
         ## sinon c'est hadoop qui s'occupe du fichier
-    filename = os.environ["defoe-robinson-103.txt"]
+    filename = os.environ["defoe_robinson_103"]
         # remove leading and trailing whitespace
     line = line.strip()
     # split the line into words
     words = line.split()
     # increase counters
     for word in words:
-        word = word.lower().strip("()!.;,:\"'")
-        if word not in stopwords:
-            key = word + ' ' + filename;
-            print('%s\t%s' % (key, 1))
+        word = word.lower().strip("\t()!.;,:\"'")
+        if word not in stopwords and word != '':
+            #key = word + '\t' + filename;
+            print('%s\t%s\t%s' % (word, 1, filename))
 
 # écriture des resultats STDOUT (standard output);
 # Cet output sera l'input de reducer_Tf_Idf.py
