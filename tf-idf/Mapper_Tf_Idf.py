835daa595904f5fcf170c08299797f2f373b6d3f
kaiiim <kkhalfao@gmail.com>
2019-03-13 14:37:46 +0000

    test0 ok

--- tf-idf/Mapper_Tf_Idf.py
+++ tf-idf/Mapper_Tf_Idf.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#! /usr/bin/env python3
 
 from __future__ import print_function
 import sys
@@ -9,24 +9,31 @@ import re
 def read_stopword(file_path):
     return (set(word.strip() for word in open(file_path)))
 
-
-stopwords = read_stopword("stopwords.txt")
+stopwords = read_stopword("/usr/local/hadoop/MapReduce/tf-idf/stopwords_en.txt")
+#stopwords = read_stopword("hdfs://NameNode:8020/user/root/stopwords_en.txt")
+#stopwords = read_stopword("./stopwords_en.txt")
+#stopwords = read_stopword("/user/root/stopwords.txt")
+#stopwords = read_stopword("/user/root/stopwords.txt")
 
 # input depuisSTDIN (standard input)
 for line in sys.stdin:
         # Pour tester en local -> export de la variable s3
         ## sinon c'est hadoop qui s'occupe du fichier
-    filename = os.environ["defoe_robinson_103.txt"]
+ 	# #  filename = os.environ["./defoe_robinson_103.txt"]
         # remove leading and trailing whitespace
-    line = line.strip()
+    #filename = os.environ["map_input_file"]
+    #print(line)    
+    #line = line.strip()
     # split the line into words
     words = line.split()
+    print("%s\t%d" % (words, 1))  
     # increase counters
-    for word in words:
-        word = word.lower().strip("\t()!.;,:\"'")
-        if word not in stopwords and word != '':
+    #for word in words:
+       # word = word.lower().strip("\t()!.;,:\"'")
+       # if word not in stopwords and word != '' and len(word) > 3:
             #key = word + '\t' + filename;
-            print('%s\t%s\t%s' % (word, 1, filename))
+            #print("")
+            #print('%s\t%s\t%s' % (word, 1, "defoe_robinson_103.txt"))
 
 # écriture des resultats STDOUT (standard output);
 # Cet output sera l'input de reducer_Tf_Idf.py
@@ -43,5 +50,3 @@ for line in sys.stdin:
 #path_file_stopword = "./tfidf/files/stopwords_en.txt"
 #fd = open(path_file_stopword, 'r') 
 #stopwords = fd.read().strip().split()
-
-
--- tf-idf/Reducer_Tf_Idf.py
+++ tf-idf/Reducer_Tf_Idf.py
@@ -1,11 +1,21 @@
+#! /usr/bin/env python3
 from operator import itemgetter
-import sys
+import sys, math
+
+i = 0 
+max = 30
 
 current_word = None
 current_count = 0
-word = None
+
+dico = {}
+data = {}
 
 for line in sys.stdin:
+    #if i < max:
+     #   print("tour", i)
+#    filename = os.environ["map_input_file"]
+#    print(filename)
     line = line.strip()
     word_stdout, count, title = line.split('\t',2)
     # cast string -> Int
@@ -13,21 +23,37 @@ for line in sys.stdin:
         count = int(count)
     except ValueError:
         continue
-    
+    data[word_stdout] = count    
     # Hadoop trie STDOUT automatiquement par key
     # Lors du tour 1 current_word == None. Attibution de current_count et current_word
     # Nous pouvons donc au tour 2 comparer automatiquement avec le current word qui réprésente
     # le résultat précédent
     if current_word == word_stdout:
-        current_count += count
+        current_count += 1
+        #if i < max:
+         #   print("current == au mot predecedentcurrent_count", current_count)
     else:
-        if current_word:
+        dico[current_word] = current_count
+        #if current_word:
             # écriture le resultat sur STDOUT
-            print('%s\t%s' % (current_word, current_count))
-        # Tour Zero attibutions du word et du count qui sera incrémenté
+            # Tour Zero attibutions du word et du count qui sera incrémenté
+            #print("x")
         current_count = count
-        current_word = word    
+        current_word = word_stdout  
+        #if i < max:
+           # print("ELSE", i,  "current_word |", current_word,"| current_count |", current_count, \
+		#"| word_stdout |",word_stdout,"| count |", count,"| titile |", title)
+    #i = i + 1
 
-# Cas de la dérniere ligne a ne pas oublier 
-if current_word == word:
-    print('%s\t%s' % (current_word, current_count))
+dico[current_word] = current_count
+nb_per_doc = len(data)
+
+
+for i, (k, v) in enumerate(dico.items()):
+    if v != 0:
+        print("keys", k, "value," ,v / nb_per_doc * math.log10(1/v)) 
+#   if i < max:
+        #print(k, v)
+#        if v != 0:
+ #           print("keys", k, "value," ,v / nb_per_doc * math.log10(1/v)) 
+
